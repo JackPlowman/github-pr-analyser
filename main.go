e165e64bebbcaceabb14b8521bf0fbe1a5df1f80
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-26 11:34:03 +0100

    update

--- main.go
+++ main.go
@@ -12,16 +12,51 @@ import (
 )
 
 func main() {
-	initLogging()
+	InitLogging()
+	CloneRepository()
 	GitHubActionSummary()
 	AddPullRequestComment("Hello World")
 }
 
 // Init logging configuration
-func initLogging() {
+func InitLogging() {
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp: true,
 	})
+
+}
+
+// Clone a GitHub repository
+func CloneRepository() {
+	fullRepoName= os.Getenv("GITHUB_REPOSITORY") // Expected format: "owner/repo"
+	repoURL := fmt.Sprintf("https://github.com/%s.git", fullRepoName)
+	cloneDir, err := MakeTemporaryDirectory()
+	if err != nil {
+		log.Fatalf("Failed to create temporary directory: %v", err)
+	}
+
+	if repoURL == "" || cloneDir == "" {
+		log.Error("Missing required environment variables: GITHUB_REPOSITORY_URL or GITHUB_CLONE_DIR")
+		return
+	}
+
+	cmd := exec.Command("git", "clone", repoURL, cloneDir)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Errorf("Failed to clone repository: %v", err)
+	} else {
+		log.Infof("Repository cloned to %s", cloneDir)
+	}
+}
+
+func MakeTemporaryDirectory() string {
+	tempDir, err := os.MkdirTemp("", "github-action-")
+	if err != nil {
+		log.Fatalf("Failed to create temporary directory: %v", err)
+	}
+	log.Infof("Temporary directory created: %s", tempDir)
+	return tempDir
 }
 
 // Generate GitHub Action Summary
